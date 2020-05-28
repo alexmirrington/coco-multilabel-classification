65591aaf7ddf67a8847dc784dda6205d48419067
Nick Rhodes <nicholasdrhodes@gmail.com>
2020-05-29 00:14:13 +1000

    small change to lstm argument default

--- code/algorithm/modules/lstm.py
+++ code/algorithm/modules/lstm.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 class BiLSTM(nn.Module):
     """BiLSTM which predicts multiple labels from caption data."""
 
-    def __init__(self, classes, threshold, embeddings_dim=100,
+    def __init__(self, classes, threshold=0.5, embeddings_dim=100,
                  hidden_size=128):
         """Initialise the BiLSTM.
 
